c2089b402b795561e99236292ab8ec999473aeef
Vartika <vartika@resolveindia.com>
2023-09-24 20:27:18 +0530

    Added sociall links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <h1 className="text-gray-400">
       <Navbar />
       <Home />
+      <SocialLinks />
     </h1>
   );
 }
